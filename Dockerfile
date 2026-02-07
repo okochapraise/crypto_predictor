bef95ccf530520138fa7f1b33ce9a01560fa66db
okochapraise <okochapraise662@gmail.com>
2026-02-08 00:35:02 +0100

    Fix Fly.io host and port binding

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,5 @@ RUN pip install --upgrade pip setuptools wheel \
 
 COPY . .
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
+CMD ["/app/.venv/bin/uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+
--- main.py
+++ main.py
@@ -56,4 +56,9 @@ def main():
     print(f"Confidence        : {final_prob:.2f}")
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000
+    )
